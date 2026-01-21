f5707c38de6cd7ff0e39b848ce6c8c328f308ad1
Charles <charles.tech0x77@outlook.com>
2026-01-20 22:56:39 -0800

    Fix/database 6292 (#7)
    * removed comment (fix/database-6292)
    
    * removed comment (fix/database-6292)
    
    * removed comment (fix/database-6292)
    
    * removed comment (fix/database-6292)

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-    packages=['userefuzz'],
-    install_requires=['requests','colorama'],
-    classifiers=[
-        'Programming Language :: Python :: 3',
     ],
     entry_points={
         'console_scripts': [
